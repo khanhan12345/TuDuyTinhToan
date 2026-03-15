4e8d0fd066bd211d2ba35a5c865968b139e6e6ac
Khanh An <vuhuua@gmail.com>
2026-03-15 20:07:49 +0700

    let me cry let me cry

--- lmc.py
+++ lmc.py
@@ -1,7 +1,22 @@
-data = input("Nhập một ký tự: ")
+#data = input("Nhập một ký tự: ")
 
 # Kiểm tra đồng thời 2 điều kiện: độ dài = 1 VÀ là chữ cái
-if len(data) == 1 and data.isalpha():
-    print(f"'{data}' là một chữ cái hợp lệ.")
-else:
-    print("Không hợp lệ! Vui lòng chỉ nhập duy nhất một chữ cái.")
+#if len(data) == 1 and data.isalpha():
+    #print(f"'{data}' là một chữ cái hợp lệ.")
+#else:
+    #print("Không hợp lệ! Vui lòng chỉ nhập duy nhất một chữ cái.")
+
+
+#x=10
+#x='now a string'
+#print(x)
+#x=1.1+2.2
+#print(isclose(x,3.3))
+
+
+#a, b, c = 2, 5, 8
+#print(a < b < c == 8)
+
+
+x  = 7 // 2 + 7 / 2
+print(x)
