c21edcabade06ceee92bfc5ac27aae377d7f9dd5
Khanh An <vuhuua@gmail.com>
2026-03-30 19:31:04 +0700

    ronaldo no world cup

---
+++ abc.py
@@ -0,0 +1,23 @@
+lst1=[5,4,8,7,6,3]
+lst2=[4,7,9,1,2,8]
+
+
+#    for i in range(6):
+      #  for j in range(6):
+       #  if lst1[i] == lst2[j]:
+               # a=[]
+                #a.append([lst1[i]])
+#print(a)
+
+#count=0
+##for i in range(len(lst)):
+n=int(input())
+a=[0,1]
+x=0
+for i in range(n):
+    x=a[n-1]+a[n-2]
+    a.append[x]
+print(x)
+
+
+
--- lmc.py
+++ lmc.py
@@ -18,5 +18,33 @@
 #print(a < b < c == 8)
 
 
-x  = 7 // 2 + 7 / 2
-print(x)
+#x  = 7 // 2 + 7 / 2
+#print(x)
+
+#nhập vào một số nguyên đại diện cho tháng trong năm(năm nhuận), in ra số ngày trong tháng đó
+#dùng matchcase:
+#thang=int(input())
+nam=int(input())
+match thang:
+    case 1|3|5|7|8|10|12:
+        print(f'thang {thang} có 31 ngày')
+    case 2:
+        if nam %400==0 or (nam%4 and nam%100!=0):
+            print(29)
+        else:
+            print(28)
+    case 4|6|9|11:
+        print(f'thang {thang} có 30 ngày')
+    case _: #truong hop con lai
+        print(f'khong co thang {thang} trong nam')
+
+
+
+
+#n=int(input())
+#match n:
+    #case n if n%2==0:
+        #print('chan')
+    #case _:
+        #print('le')
+
